4598b9d215e355c13c3be9accb11bc090b4fcbb7
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-02-07 12:23:40 -0300

    Empty project

---
+++ src/main/java/com/example/sqliteflyway/SqliteFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqliteflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqliteFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqliteFlywayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sqliteflyway/SqliteFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqliteflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqliteFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
