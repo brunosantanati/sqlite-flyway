bd03dd4f69ff62fd0ef9707a5a544cd0e0b41ab9
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-02-07 13:05:43 -0300

    Make the example work using Sqlite

--- src/main/java/com/example/sqliteflyway/SqliteFlywayApplication.java
+++ src/main/java/com/example/sqliteflyway/SqliteFlywayApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SqliteFlywayApplication {
 
+	/*
+	I had to create de DB before running this program, in order to make it work:
+	sqlite> CREATE TABLE person (id integer not null, message varchar, name varchar, primary key (id));
+	 */
+
 	public static void main(String[] args) {
 		SpringApplication.run(SqliteFlywayApplication.class, args);
 	}
